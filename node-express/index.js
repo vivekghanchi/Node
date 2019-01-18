dfe861e4f2a8ac6ee0bc4ac998674a92b38d3d6a
vivek.seemly <vivek.seemly@gmail.com>
2019-01-18 18:21:39 +0530

    Express Simple Rest

--- node-express/index.js
+++ node-express/index.js
@@ -1,18 +1,69 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname+ '/public'));
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+app.get('/dishes', (req, res, next) => {
+    res.end("Will send all the dishes to you!!!");
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end("Will add the dish: " + req.body.name + "  with details : " + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put Operation is not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes!!!!!');
+});
+
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end("Will send detail of the dish : " + req.params.dishId +  "to you");
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post Operation is not supported on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId);
+    res.end('Will update the dish: ' + req.body.name + "With details: " + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
+
+
+
+
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/plain');
     res.end('<html><body><h1> This is an Express Server </h1></body></html>');
 });
 
