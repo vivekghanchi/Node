7a0aff6ce4fab12eeae289bb34656d44ecf322d2
vivek.seemly <vivekghanchi12345@gmail.com>
2019-01-28 09:34:46 +0530

    Mongoose part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -11,15 +11,14 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'   
-    });
-
-    newDish.save().then((dish) => {
+    Dishes.create({
+            name: 'Uthappizza',
+            description: 'test'
+        })
+        .then((dish) => {
             console.log(dish);
 
-           return  Dishes.find({}).exec();
+            return Dishes.find({}).exec();
         })
         .then((dishes) => {
             console.log(dishes);
