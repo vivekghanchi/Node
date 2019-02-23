34eec75a3a3eeec4b0c1450ce8356201f4104c64
vivek.seemly <vivekghanchi12345@gmail.com>
2019-02-23 10:26:20 +0530

    Passport JWT

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../model/user');
 var passport = require('passport');
+var authenticate = require('../authenticate');
 var router = express.Router();
 router.use(bodyParser.json());
 
@@ -14,6 +15,7 @@ router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
+      console.log(err);
       res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json');
       res.json({err: err});
@@ -29,9 +31,10 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res) => {
